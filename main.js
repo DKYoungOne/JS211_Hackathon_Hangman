b20a5ef57acfdf2694545b221b5f982de15eea12
DKYoungOne <bawsinator@gmail.com>
2023-02-27 15:24:06 -0600

    Working on integrating hangman function to DOM

--- main.js
+++ main.js
@@ -7,18 +7,25 @@ const hideWord = (wrd) => {
   for (let i = 0; i < wrd.length; i++) {
     wordShown.push('-');
   }
-
-  return wordShown;
+  document.getElementById("wordProgress").innerHTML = wordShown;
+  return wordShown.join("");
 }
 
 const filterLetter = (letter, word) => {
-  
+  let cachedAnswer = wordShown;
   
   for (let i = 0; i < word.length; i++) {
     if (word[i] === letter) {
       wordShown[i] = word[i];
-    } 
-    
+    }
   }
+  if (cachedAnswer === wordShown) {
+      document.getElementById("playerFeedback").innerHTML = "Try another letter.";
+    }
   return wordShown.join("");
+}
+
+
+const hangman = () => {
+  
 }
